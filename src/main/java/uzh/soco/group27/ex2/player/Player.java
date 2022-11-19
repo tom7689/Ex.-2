1773e1b2cea40abfe80c6283d56c73be67ef6af2
tom7689 <tomkaspar.meier@uzh.ch>
2022-11-19 12:24:45 +0100

    player

--- src/main/java/uzh/soco/group27/ex2/player/Player.java
+++ src/main/java/uzh/soco/group27/ex2/player/Player.java
@@ -1,4 +1,32 @@
 package uzh.soco.group27.ex2.player;
 
-public class Player {
+import javax.smartcardio.Card;
+
+public class Player implements Players {
+    private int score =0;
+    private String name;
+    public Player(String name){
+        this.name = name;
+
+    }
+
+    @Override
+    public Card drawCard() {
+        return null;
+    }
+
+    @Override
+    public int rollDice() {
+        return 0;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public void chooseDice() {
+
+    }
 }
--- src/main/java/uzh/soco/group27/ex2/player/Players.java
+++ src/main/java/uzh/soco/group27/ex2/player/Players.java
@@ -1,4 +1,12 @@
 package uzh.soco.group27.ex2.player;
 
+import javax.smartcardio.Card;
+
 public interface Players {
+    Card drawCard();
+    int rollDice();
+    int getScore();
+    void chooseDice();
+
+
 }
