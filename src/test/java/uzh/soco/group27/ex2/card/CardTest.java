23c6b4e5663bfaad6414556a0f5c32f45d7169a8
flolanz <florian.lanz@uzh.ch>
2022-12-03 02:37:23 +0100

    testing

--- src/test/java/uzh/soco/group27/ex2/card/CardTest.java
+++ src/test/java/uzh/soco/group27/ex2/card/CardTest.java
@@ -1,4 +1,57 @@
-package uzh.soco.group27.ex2.testing.card;
+package uzh.soco.group27.ex2.card;
+
+import org.junit.jupiter.api.Test;
+import uzh.soco.group27.ex2.card.Card;
+import uzh.soco.group27.ex2.card.Cloverleaf;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardTest {
+
+    @Test
+    public void isCloverleaf() {
+        assertEquals(Cloverleaf.class, Card.get(0).getClass());
+    }
+
+    @Test
+    public void isFireworks() {
+        for (int i = 1; i < 6; i++) {
+            assertEquals(Fireworks.class, Card.get(i).getClass());
+        }
+    }
+
+    @Test
+    public void isStop() {
+        for (int i = 6; i < 16; i++) {
+            assertEquals(Stop.class, Card.get(i).getClass());
+        }
+    }
+
+    @Test
+    public void isStraight() {
+        for (int i = 16; i < 21; i++) {
+            assertEquals(Straight.class, Card.get(i).getClass());
+        }
+    }
+
+    @Test
+    public void isPlusMinus() {
+        for (int i = 21; i < 26; i++) {
+            assertEquals(PlusMinus.class, Card.get(i).getClass());
+        }
+    }
+
+    @Test
+    public void isX2() {
+        for (int i = 26; i < 31; i++) {
+            assertEquals(X2.class, Card.get(i).getClass());
+        }
+    }
+
+    @Test
+    public void isBonus() {
+        for (int i = 31; i < 56; i++) {
+            assertEquals(Bonus.class, Card.get(i).getClass());
+        }
+    }
 }
